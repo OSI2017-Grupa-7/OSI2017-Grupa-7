b85599f5af692aa7da37fbf15fbb5e8b3c7eca42
Susnjar <Susnjar@DESKTOP-ICDGL53>
2017-12-09 15:16:03 +0100

    Fajlovi koji ce se koristiti za izvjestaje

---
+++ TurboTotal/Report.cpp
@@ -0,0 +1,19 @@
+#include "Report.h"
+
+void buyerReport()
+{
+	system("cls");
+	std::cout << "PODACI O KUPCU" << std::endl;
+}
+
+void productReport()
+{
+	system("cls");
+	std::cout << "PODACI O ARTIKLU" << std::endl;
+}
+
+void monthSaldoReport()
+{
+	system("cls");
+	std::cout << "PODACI O MJESECU" << std::endl;
+}
---
+++ TurboTotal/Report.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Functions.h"
+
+void buyerReport();
+void productReport();
+void monthSaldoReport();
