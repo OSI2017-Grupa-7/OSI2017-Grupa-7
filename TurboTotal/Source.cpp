2bd8c82fd0aa61d6dbf9516233ced7de1ca1cc12
Susnjar <Susnjar@DESKTOP-ICDGL53>
2017-12-09 15:22:25 +0100

    Okvirni izgled Source fajla

--- TurboTotal/Source.cpp
+++ TurboTotal/Source.cpp
@@ -1,8 +1,18 @@
-#include "Person.h"
+#include "Report.h"
+//#include "Pretprogram.h"
+//#include "AdminFunctionals.h"
+//#include "AnalystFunctionals.h"
+
+int kraj = 1;
 
 int main()
 {
-	std::cout << "TURBO TOTAL";
-	getchar();
-	getchar();
+	//Pretprogram();
+	while (kraj)
+	{
+		short option;
+		AdminAnalystChoose(option);
+		//if (option == 1) { AdminFunctionals(); }
+		//if (option == 2) { AnalystFunctionals(); }
+	}
 }
