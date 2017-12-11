2243b8aed763891969ef093dcb2808493d4eb8f2
Susnjar <Susnjar@DESKTOP-ICDGL53>
2017-12-11 20:37:51 +0100

    pretpostavljeni izgled prilikom izvjestaja (mn.) datoteka: Report.cpp u sve tri funkcije je obavljena izmjena

--- TurboTotal/Report.cpp
+++ TurboTotal/Report.cpp
@@ -3,17 +3,30 @@
 void buyerReport()
 {
 	system("cls");
-	std::cout << "PODACI O KUPCU" << std::endl;
+	std::string ime, prezime;
+	std::cout << "IZVJESTAJ PO KUPCU" << std::endl << std::endl;
+	std::cout << "Unesite ime i prezime kupca: "<<std::endl;
+	std::cout << std::endl << "Ime: ";  std::cin >> ime;
+	std::cout << std::endl << "Prezime: "; std::cin >> prezime;
+	//uci u bazu kupaca i ispisati izvjestaj
 }
 
 void productReport()
 {
 	system("cls");
-	std::cout << "PODACI O ARTIKLU" << std::endl;
+	std::cout << "IZVJESTAJ PO ARTIKLU" << std::endl<<std::endl;
+	std::string naziv;
+	std::cout << "IZVJESTAJ PO KUPCU" << std::endl << std::endl;
+	std::cout << "Unesite naziv artikla: " << std::endl;
+	std::cout << std::endl << "Naziv: ";  std::cin >> naziv;
+	//uci u bazu artikala i ispisati izvjestaj
 }
 
 void monthSaldoReport()
 {
 	system("cls");
-	std::cout << "PODACI O MJESECU" << std::endl;
+	std::string mjesec;
+	std::cout << "MJESECNI IZVJESTAJ" << std::endl<<std::endl;
+	std::cout << "Unesite naziv mjeseca: ";std::cin>>mjesec;
+	//uci u bazu artikala i ispisati izvjestaj
 }
