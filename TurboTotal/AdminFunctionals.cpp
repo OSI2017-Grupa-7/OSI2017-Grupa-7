b9dff4e8dc10c85a5120e5a9b171b582e358821b
keks <keks@keks-PC>
2017-12-15 18:59:02 +0100

    odobravanje pristupa sistemu, promjena ID-ova, brisanje malicioznih kor.

--- TurboTotal/AdminFunctionals.cpp
+++ TurboTotal/AdminFunctionals.cpp
@@ -345,9 +345,299 @@ void AdminFunctionals()
 								}
 							}
 						}
+						/////////////////odobravanje pristupa sistemu
+						if (option1 == 4)
+						{
+							short user;
+							do
+							{
+								system("cls");
+								std::cout << "ODOBRAVANJE PRISTUPA SISTEMU" << std::endl;
+								std::cout << std::endl << "1. Odobravanje pristupa administratoru" << std::endl;
+								std::cout << "2. Odobravanje pristupa analiticaru" << std::endl;
+								std::cout << std::endl << "Unesi opciju: "; std::cin >> user;
+							} while (user != 1 && user != 2);
+							if (user == 1)
+							{
+								/////////////promjena statusa admina za odrenjeni ID
+								system("cls");
+								std::string id;
+								char name[15], surname[15], ID[15], PIN[15], maliciozan[15];
+								FILE* myfile;
+								std::cout << "ODOBRAVANJE PRISTUPA ADMINISTRATORU" << std::endl;
+
+								FILE* d;
+								d = fopen("MaliciozniAdmini.txt", "r");
+								std::vector<Person> vec;
+								while (fscanf(d, "%s %s %s %s %s\n", name, surname, ID, PIN, maliciozan) == 5)
+								{
+									Person b;
+									std::string nname(name); b.setName(nname);
+									std::string ssurname(surname); b.setSurname(ssurname);
+									std::string PPIN(PIN); b.setPIN(PPIN);
+									std::string IID(ID); b.setID(IID);
+									std::string mmaliciozan(maliciozan); b.setMalicious(mmaliciozan);
+									vec.push_back(b);
+								}
+								fclose(d);
+								if (vec.size())
+								{
+									std::cout << std::endl << "Spisak malicioznih admina: " << std::endl;
+									printPersonHeader();
+									for (int i = 0; i < vec.size(); i++)
+										std::cout << i + 1 << ". " << std::setw(15) << std::left << vec[i].getName() << std::setw(15) << std::left << vec[i].getSurname() << std::setw(10) << std::left << vec[i].getID() << std::setw(10) << std::left << vec[i].getPIN() << std::setw(10) << std::left << vec[i].getMalicious() << std::endl;
+									printPersonFooter();
+
+									std::cout << std::endl << "Unesite ID administratora kojem zelite odobriti pristup: "; std::cin >> id;
+									if (isThereMaliciousAdmin(id))
+									{
+										std::cout << std::endl << "Administratoru sa unesenim ID-om je omoguceno pravo pristupa sistemu." << std::endl;
+										FILE* file;
+										file = fopen("RegistrovaniAdmini.txt", "r");
+										std::vector<Person> v;
+										while (fscanf(file, "%s %s %s %s %s\n", name, surname, ID, PIN, maliciozan) == 5)
+										{
+											Person b;
+											std::string nname(name); b.setName(nname);
+											std::string ssurname(surname); b.setSurname(ssurname);
+											std::string PPIN(PIN); b.setPIN(PPIN);
+											std::string IID(ID); b.setID(IID);
+											std::string mmaliciozan(maliciozan); b.setMalicious(mmaliciozan);
+											v.push_back(b);
+										}
+
+										for (int i = 0; i < v.size(); i++)
+											if (v[i].getID() == id)
+											{
+												v[i].getMalicious() = "odobren";
+											}
+
+										fclose(file);
+										std::ofstream f;
+										f.open("RegistrovaniAdmini.txt");
+										for (int i = 0; i < v.size(); i++)
+											f << std::setw(15) << std::left << v[i].getName() << std::setw(15) << std::left << v[i].getSurname() << std::setw(10) << std::left << v[i].getID() << std::setw(10) << std::left << v[i].getPIN() << std::setw(10) << std::left << v[i].getMalicious() << std::endl;
+										f.close();
+
+
+
+										/////////////////brisanje malicioznih admina iz MaliciozniAdmini.txt
+										for (int i = 0; i < vec.size(); i++)
+											if (vec[i].getID() == id)
+											{
+												vec.erase(vec.begin() + i);
+											}
+										std::ofstream filee;
+										filee.open("MaliciozniAdmini.txt");
+										for (int i = 0; i < vec.size(); i++)
+											filee << std::setw(15) << std::left << vec[i].getName() << std::setw(15) << std::left << vec[i].getSurname() << std::setw(10) << std::left << vec[i].getID() << std::setw(10) << std::left << vec[i].getPIN() << std::setw(10) << std::left << vec[i].getMalicious() << std::endl;
+										filee.close();
+									}
+									else
+									{
+										std::wcout << std::endl << "Ne postoji maliciozan administrator sa unesenim ID-om!" << std::endl;
+									}
+								}
+								else
+								{
+									std::cout << std::endl << "Nijedan od administratora nije maliciozan." << std::endl;
+								}
+							}
+							if (user == 2)
+							{
+								/////////////promjena statusa analiticara za odrenjeni ID
+								system("cls");
+								std::string id;
+								char name[15], surname[15], ID[15], PIN[15], maliciozan[15];
+								FILE* myfile;
+								std::cout << "ODOBRAVANJE PRISTUPA ANALITICARU" << std::endl;
+
+								FILE* d;
+								d = fopen("MaliciozniAnaliticari.txt", "r");
+								std::vector<Person> vec;
+								while (fscanf(d, "%s %s %s %s %s\n", name, surname, ID, PIN, maliciozan) == 5)
+								{
+									Person b;
+									std::string nname(name); b.setName(nname);
+									std::string ssurname(surname); b.setSurname(ssurname);
+									std::string PPIN(PIN); b.setPIN(PPIN);
+									std::string IID(ID); b.setID(IID);
+									std::string mmaliciozan(maliciozan); b.setMalicious(mmaliciozan);
+									vec.push_back(b);
+								}
+								fclose(d);
+								if (vec.size())
+								{
+									std::cout << std::endl << "Spisak malicioznih analiticara: " << std::endl;
+									printPersonHeader();
+									for (int i = 0; i < vec.size(); i++)
+										std::cout << i + 1 << ". " << std::setw(15) << std::left << vec[i].getName() << std::setw(15) << std::left << vec[i].getSurname() << std::setw(10) << std::left << vec[i].getID() << std::setw(10) << std::left << vec[i].getPIN() << std::setw(10) << std::left << vec[i].getMalicious() << std::endl;
+									printPersonFooter();
+
+									std::cout << std::endl << "Unesite ID analiticara kojem zelite odobriti pristup: "; std::cin >> id;
+
+									if (isThereMaliciousAnalyst(id))
+									{
+										std::cout << std::endl << "Analiticaru sa unesenim ID-om je omoguceno pravo pristupa sistemu." << std::endl;
+										FILE* file;
+										file = fopen("RegistrovaniAnaliticari.txt", "r");
+										std::vector<Person> v;
+										while (fscanf(file, "%s %s %s %s %s\n", name, surname, ID, PIN, maliciozan) == 5)
+										{
+											Person b;
+											std::string nname(name); b.setName(nname);
+											std::string ssurname(surname); b.setSurname(ssurname);
+											std::string PPIN(PIN); b.setPIN(PPIN);
+											std::string IID(ID); b.setID(IID);
+											std::string mmaliciozan(maliciozan); b.setMalicious(mmaliciozan);
+											v.push_back(b);
+										}
+
+										for (int i = 0; i < v.size(); i++)
+											if (v[i].getID() == id)
+											{
+												v[i].getMalicious() = "odobren";
+											}
+
+										fclose(file);
+										std::ofstream f;
+										f.open("RegistrovaniAnaliticari.txt");
+										for (int i = 0; i < v.size(); i++)
+											f << std::setw(15) << std::left << v[i].getName() << std::setw(15) << std::left << v[i].getSurname() << std::setw(10) << std::left << v[i].getID() << std::setw(10) << std::left << v[i].getPIN() << std::setw(10) << std::left << v[i].getMalicious() << std::endl;
+										f.close();
+
+										/////////////////brisanje malicioznih analiticara iz MaliciozniAnaliticari.txt
+										for (int i = 0; i < vec.size(); i++)
+											if (vec[i].getID() == id)
+											{
+												vec.erase(vec.begin() + i);
+											}
+
+										std::ofstream filee;
+										filee.open("MaliciozniAnaliticari.txt");
+										for (int i = 0; i < vec.size(); i++)
+											filee << std::setw(15) << std::left << vec[i].getName() << std::setw(15) << std::left << vec[i].getSurname() << std::setw(10) << std::left << vec[i].getID() << std::setw(10) << std::left << vec[i].getPIN() << std::setw(10) << std::left << vec[i].getMalicious() << std::endl;
+										filee.close();
+									}
+									else
+									{
+										std::wcout << std::endl << "Ne postoji maliciozan analiticar sa unesenim ID-om!" << std::endl;
+									}
+								}
+
+								else
+								{
+									std::cout << std::endl << "Nijedan od analiticara nije maliciozan." << std::endl;
+								}
+							}
+						}
+
 					}
+					do
+					{
+						std::cout << std::endl << "0. Odjavi se" << std::endl;
+						std::cout << "1. Vrati se na pocetak profila" << std::endl;
+						std::cout << "2. Izadji iz aplikacije" << std::endl;
+						std::cout << std::endl << "Unesi opciju: "; std::cin >> t;
+						if (t == 0) kraj = 1;
+						if (t == 2)
+						{
+							kraj = 0;
+							t = 0;
+						}
+					} while (t != 0 && t != 1 && t != 2);
 				}
+			}
+			else
+			{
+				std::cout << std::endl << "ID ili sifra nisu korektno uneseni. Broj preostalih pokusaja: " << --i << " (pritisni 'Enter' za sledeci pokusaj)";
+				getchar();
+				getchar();
 			}
 		}
 	}
+
+	if (!i && (p == 2 || p == 3))
+	{
+		if (p == 2)
+		{
+			std::string pom = a.getID();
+			char name[15], surname[15], ID[15], PIN[15], maliciozan[15];
+			FILE* myfile;
+			myfile = fopen("RegistrovaniAdmini.txt", "r");
+			std::vector <Person> v;
+			Person b;
+			while (fscanf(myfile, "%15s %15s %10s %10s %15s\n", name, surname, ID, PIN, maliciozan) == 5)
+			{
+				std::string nname(name); b.setName(nname);
+				std::string ssurname(surname); b.setSurname(ssurname);
+				std::string IID(ID); b.setID(IID);
+				std::string PPIN(PIN); b.setPIN(PPIN);
+				std::string mmaliciozan(maliciozan); b.setMalicious(mmaliciozan);
+				v.push_back(b);
+			}
+			fclose(myfile);
+			for (int i = 0; i < v.size(); i++)
+			{
+				if (v[i].getID() == pom)
+				{
+					v[i].getMalicious() = "onemogucen";
+					if (maliciousAdmin(v[i]))
+					{
+						std::ofstream f;
+						f.open("MaliciozniAdmini.txt", std::ios::app);
+						f << std::setw(15) << std::left << v[i].getName() << std::setw(15) << std::left << v[i].getSurname() << std::setw(10) << std::left << v[i].getID() << std::setw(10) << std::left << v[i].getPIN() << std::setw(10) << std::left << v[i].getMalicious() << std::endl;
+						f.close();
+					}
+				}
+			}
+			std::ofstream file;
+			file.open("RegistrovaniAdmini.txt");
+			for (int i = 0; i < v.size(); i++)
+				file << std::setw(15) << std::left << v[i].getName() << std::setw(15) << std::left << v[i].getSurname() << std::setw(10) << std::left << v[i].getID() << std::setw(10) << std::left << v[i].getPIN() << std::setw(10) << std::left << v[i].getMalicious() << std::endl;
+			file.close();
+		}
+		if (p == 3)
+		{
+			std::string pom = a.getPIN();
+			char name[15], surname[15], ID[15], PIN[15], maliciozan[15];
+			FILE* myfile;
+			myfile = fopen("RegistrovaniAdmini.txt", "r");
+			std::vector <Person> v;
+			Person b;
+			while (fscanf(myfile, "%15s %15s %10s %10s %15s\n", name, surname, ID, PIN, maliciozan) == 5)
+			{
+				std::string nname(name); b.setName(nname);
+				std::string ssurname(surname); b.setSurname(ssurname);
+				std::string IID(ID); b.setID(IID);
+				std::string PPIN(PIN); b.setPIN(PPIN);
+				std::string mmaliciozan(maliciozan); b.setMalicious(mmaliciozan);
+				v.push_back(b);
+			}
+			fclose(myfile);
+			for (int i = 0; i < v.size(); i++)
+			{
+				if (v[i].getPIN() == pom)
+				{
+					v[i].getMalicious() = "onemogucen";
+					if (maliciousAdmin(v[i]))
+					{
+						std::ofstream f;
+						f.open("MaliciozniAdmini.txt", std::ios::app);
+						f << std::setw(15) << std::left << v[i].getName() << std::setw(15) << std::left << v[i].getSurname() << std::setw(10) << std::left << v[i].getID() << std::setw(10) << std::left << v[i].getPIN() << std::setw(10) << std::left << v[i].getMalicious() << std::endl;
+						f.close();
+					}
+				}
+			}
+			std::ofstream file;
+			file.open("RegistrovaniAdmini.txt");
+			for (int i = 0; i < v.size(); i++)
+				file << std::setw(15) << std::left << v[i].getName() << std::setw(15) << std::left << v[i].getSurname() << std::setw(10) << std::left << v[i].getID() << std::setw(10) << std::left << v[i].getPIN() << std::setw(10) << std::left << v[i].getMalicious() << std::endl;
+			file.close();
+		}
+		system("cls");
+		std::cout << "Pogresno ste unijeli podatke za prijavu 3x." << std::endl;
+		std::cout << std::endl << "Da biste opet imali pristup sistemu, kontaktirajte administratora." << std::endl;
+	}
+	getchar();
 }
