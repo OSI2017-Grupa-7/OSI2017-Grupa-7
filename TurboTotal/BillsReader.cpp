29a5bc435646a6bc8a54e7dbd1eeeae810d3e65d
Susnjar <Susnjar@DESKTOP-ICDGL53>
2017-12-15 20:02:55 +0100

    okvirni izgled funkcije za ucitavanje racuna

--- TurboTotal/BillsReader.cpp
+++ TurboTotal/BillsReader.cpp
@@ -1,6 +1,37 @@
 #include "BillsReader.h"
 
-void billsReading() 
+std::vector<std::string> get_filenames(std::experimental::filesystem::path path)
 {
-	//funkcija koja procita sve racune, obradi ih i smjesti gdje treba
+	namespace stdfs = std::experimental::filesystem;
+
+	std::vector<std::string> filenames;
+	const stdfs::directory_iterator end{};
+
+	for (stdfs::directory_iterator iter{ path }; iter != end; ++iter)
+	{
+		if (stdfs::is_regular_file(*iter)) // comment out if all names (names of directories tc.) are required
+			filenames.push_back(iter->path().string());
+	}
+
+	return filenames;
+}
+
+void billsReading()
+{
+	std::vector<std::string> bills = get_filenames("C:/Users/Susnjar/Desktop/Projektni_zadatak/TurboTotal/Racuni");
+	for (unsigned int i = 0; i < bills.size(); i++)
+	{
+		std::string line;
+		std::ifstream file(bills[i]);
+		std::cout << std::endl << "=====================================================" << std::endl;
+		std::cout << "Racun: " << i + 1;
+		std::cout << std::endl << "=====================================================" << std::endl;
+		while (getline(file, line))
+		{
+			std::cout << line<<std::endl;
+		}
+	}
+	
+	getchar();
+	getchar();
 }
--- TurboTotal/BillsReader.h
+++ TurboTotal/BillsReader.h
@@ -1,3 +1,10 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <experimental/filesystem>
 
+
+std::vector<std::string> get_filenames(std::experimental::filesystem::path);
 void billsReading();
