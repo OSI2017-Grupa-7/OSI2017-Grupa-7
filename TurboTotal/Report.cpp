ad992aa0156dee8378c06cfbedb3651f19a9d546
Susnjar <Susnjar@DESKTOP-ICDGL53>
2017-12-17 20:13:12 +0100

    napravljen 'Meni' za mjesecni izvjestaj

--- TurboTotal/Report.cpp
+++ TurboTotal/Report.cpp
@@ -26,10 +26,91 @@ void productReport()
 
 void monthSaldoReport()
 {
-	system("cls");
-	logoS();
 	std::string mjesec;
-	std::cout <<std::endl<< "MJESECNI IZVJESTAJ" << std::endl<<std::endl;
-	std::cout << "Unesite naziv mjeseca: ";std::cin>>mjesec;
-	//uci u bazu artikala i ispisati izvjestaj
+	do
+	{
+
+		system("cls");
+	logoS();
+	std::cout << std::endl << "MJESECNI IZVJESTAJ" << std::endl << std::endl;
+	std::cout << std::endl << " 1. Januar          2. Februar         3. Mart" << std::endl;
+	std::cout << " 4. April           5. Maj             6. Jun" << std::endl;
+	std::cout << " 7. Jul             8. Avgust          9. Septembar" << std::endl;
+	std::cout << "10. Oktobar        11. Novembar       12.Decembar" << std::endl;
+	std::cout << std::endl << "Unesi opciju: "; std::cin >> mjesec;
+	} while (mjesec != "1" && mjesec != "2" && mjesec != "3" && mjesec != "4" && mjesec != "5" && mjesec != "6" &&
+		mjesec != "7" && mjesec != "8" && mjesec != "9" && mjesec != "10" && mjesec != "11" && mjesec != "12");
+	if (mjesec == "1")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "JANUAR"<<std::endl;
+	}
+	if (mjesec == "2")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "FEBRUAR" << std::endl;
+	}
+	if (mjesec == "3")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "MART" << std::endl;
+	}
+	if (mjesec == "4")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "APRIL" << std::endl;
+	}
+	if (mjesec == "5")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "MAJ" << std::endl;
+	}
+	if (mjesec == "6")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "JUN" << std::endl;
+	}
+	if (mjesec == "7")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "JUL" << std::endl;
+	}
+	if (mjesec == "8")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "AVGUST" << std::endl;
+	}
+	if (mjesec == "9")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "SEPTEMBAR" << std::endl;
+	}
+	if (mjesec == "10")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "OKROBAR" << std::endl;
+	}
+	if (mjesec == "11")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "NOVEMBAR" << std::endl;
+	}
+	if (mjesec == "12")
+	{
+		system("cls");
+		logoS();
+		std::cout << std::endl << "DECEMBAR" << std::endl;
+	}
+//uci u bazu artikala i ispisati izvjestaj
 }
