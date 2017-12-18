248b2fa288d21bed567f42a5ac58ddf10f7416b4
Bicba <Korisnik@TDsteel>
2017-12-18 13:36:27 +0100

    Bicbaaa

--- TurboTotal/Source.cpp
+++ TurboTotal/Source.cpp
@@ -2,18 +2,22 @@
 #include "Pretprogram.h"
 #include "AdminFunctionals.h"
 #include "AnalystFunctionals.h"
+#include "BillsReader.h"
 
 
 int kraj = 1;
 
 int main()
 {
-	Pretprogram();
-	while (kraj)
+	//Pretprogram();
+
+	billFormat();
+
+	/*while (kraj)
 	{
 		char option;
 		AdminAnalystChoose(option);
 		if (option == '1') { AdminFunctionals(); }
 		if (option == '2') { AnalystFunctionals(); }
-	}
+	}*/
 }
