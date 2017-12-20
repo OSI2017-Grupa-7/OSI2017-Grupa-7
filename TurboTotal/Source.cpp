101c1762098aea8ce117cc0fbc5e5ff507a5af09
Susnjar <Susnjar@DESKTOP-ICDGL53>
2017-12-20 15:21:39 +0100

    dodano da se pri svakom pokretanju par sekundi stoji samo Logo

--- TurboTotal/Source.cpp
+++ TurboTotal/Source.cpp
@@ -2,12 +2,20 @@
 #include "Pretprogram.h"
 #include "AdminFunctionals.h"
 #include "AnalystFunctionals.h"
-
-
+#include "Logo.h"
+#include <cstdlib>
 int kraj = 1;
 
 int main()
 {
+	system("MODE CON COLS=201 LINES=70");
+	startLogo();
+	int i = 0;
+	while (i < 1000000000)
+	{
+		i++;
+	} 
+	system("cls");
 	Pretprogram();
 	while (kraj)
 	{
