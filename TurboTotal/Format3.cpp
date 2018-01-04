675e321b145589e935b4a42f9249c8e871d7dc95
keks <keks@keks-PC>
2018-01-04 12:39:25 +0100

    premjestanje fajlova u odredjene foldere

--- TurboTotal/Format3.cpp
+++ TurboTotal/Format3.cpp
@@ -30,13 +30,21 @@ void boi(std::string& trash, std::string& st2, double& kol, int& i) {
 
 
 void format3(std::string& bill) {
-	std::ifstream file(bill);
+	std::ifstream file("Racuni/" + bill);
 	file.seekg(0, file.beg);
-	std::string trash, st1, st2, st;
+	std::string trash, st1, st2;
+	std::vector<std::string> st;
 	double kol, cijena, ukupno;
+	std::vector<double> kolV, cijenaV, ukupnoV;
 	std::string kupac, datum;
 	double total, pdv, totalp;
 	int day, month, year;
+	int count = 0;
+	double sum = 0;
+	std::string rac = "Racuni/" + bill, err = "Error/" + bill, obr = "Obradjeni_racuni/" + bill;
+	const char* rac_path = rac.c_str();
+	const char* err_path = err.c_str();
+	const char* obr_path = obr.c_str();
 
 	GotoLine(file, 3);
 	file >> trash >> kupac;
@@ -51,14 +59,16 @@ void format3(std::string& bill) {
 	file >> trash >> trash >> trash >> totalp;
 	file.seekg(0, file.beg);
 	
+	if (totalp != total + pdv) {
+		file.close();
+		rename(rac_path, err_path);
+		return;
+	}
 
 	for (int i = 0; i < 9; i++)
 		std::getline(file, trash);
 
 	parseDate(datum, day, month, year);
-
-	std::ofstream kupci("Kupci/" + kupac + ".txt", std::ios::app);
-	kupci << day << "." << month << "." << "year" << std::endl;
 
 	while (file >> st1 >> trash && st1[0] != '-') {
 
@@ -70,19 +80,35 @@ void format3(std::string& bill) {
 			i++;
 		}
 
-		st = st2 + ' ' + st1;
+		st.push_back(st2 + ' ' + st1);
+		count++;
 
 		boi(trash, st2, kol, i);
 		boi(trash, st2, cijena, i);
 		boi(trash, st2, ukupno, i);
 
-		kupci << st << " " << kol << " " << cijena << " " << ukupno << std::endl;
+		kolV.push_back(kol); cijenaV.push_back(cijena); ukupnoV.push_back(ukupno);
 		trash.clear();
 		st2.clear();
 	}
 
+	for (int i = 0; i < count; i++)
+		sum = sum + ukupnoV[i];
+
+	if (sum != total) {
+		file.close();
+		rename(rac_path, err_path);
+		return;
+	}
+
+	std::ofstream kupci("Kupci/" + kupac + ".txt", std::ios::app);
+
+	for (int i = 0; i < count; i++)
+		kupci << st[i] << " " << kolV[i] << " " << cijenaV[i] << " " << ukupnoV[i] << std::endl;
+	kupci << day << "." << month << "." << "year" << std::endl;
 	kupci << total << std::endl << pdv << std::endl << totalp << std::endl;
 	kupci << "--------------------------" << std::endl;
 	file.close();
 	kupci.close();
+	rename(rac_path, obr_path);
 }
