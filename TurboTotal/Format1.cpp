33333f3c719791b7b4c8e56bd01c7f81addc3977
Bicba <34394144+Nikola-Babic@users.noreply.github.com>
2018-01-20 18:21:43 +0100

    Popravka Srpskih Izraza u Formatu 1

--- TurboTotal/Format1.cpp
+++ TurboTotal/Format1.cpp
@@ -5,58 +5,58 @@
 using std::string;
 
 struct Artikal {
-	string proizvod, sifra, kolicina, cijena,ukupno; //pomocna struktura,napravljena radi lakse formatiranog upisa
+	string AArticle, ACode, AQuant, APrice,ATotal; //pomocna struktura,napravljena radi lakse formatiranog upisa
 	
 };
 
 bool checkError(string& file) // funkcija za provjeru greski
 {
-	string pom,racUkupno, PDV, ukupPDV,pom2;
+	string tmp,billTotal, PDV, billTotalPDV,tmp2;
 	int flag = 1,errflag=0; Artikal art;
 	double kol, cjn, ukp,sum=0,sumR,sumPDV,sumUkup;
 	std::ifstream fp("Racuni/" + file); //otvaranje fajla koji sadrzi racune
-	string pomerr = file;
-	string errorfilestring = "Error/" + pomerr.erase(pomerr.length() - 4, pomerr.length()) + "_error" + ".txt";
-	std::getline(fp, pom);
-    std::getline(fp, pom);
-	std::getline(fp, pom);
-	std::getline(fp, pom);
-	std::getline(fp, pom); // preskakanje linija u racunu,koje ne sadrze bitne informacije
+	string tmperr = file;
+	string errorfilestring = "Error/" + tmperr.erase(tmperr.length() - 4, tmperr.length()) + "_error" + ".txt";
+	std::getline(fp, tmp);
+    std::getline(fp, tmp);
+	std::getline(fp, tmp);
+	std::getline(fp, tmp);
+	std::getline(fp, tmp); // preskakanje linija u racunu,koje ne sadrze bitne informacije
 	while (flag == 1) // ucitavanje artikala jedan po jedan
 	{
-		std::getline(fp, pom);
-		fp >> art.proizvod;
-		if (art.proizvod == "---------------------------------------")break;
-		fp >> art.sifra;
-		fp >> pom2;
-		fp >> art.kolicina;
-		fp >> pom2;
-		fp >> art.cijena;
-		fp >> pom2;
-		fp >> art.ukupno;
-		kol = std::stod(art.kolicina);
-		cjn = std::stod(art.cijena);
-		ukp = std::stod(art.ukupno);
-		sum = sum + std::stod(art.ukupno);
+		std::getline(fp, tmp);
+		fp >> art.AArticle;
+		if (art.AArticle == "---------------------------------------")break;
+		fp >> art.ACode;
+		fp >> tmp2;
+		fp >> art.AQuant;
+		fp >> tmp2;
+		fp >> art.APrice;
+		fp >> tmp2;
+		fp >> art.ATotal;
+		kol = std::stod(art.AQuant);
+		cjn = std::stod(art.APrice);
+		ukp = std::stod(art.ATotal);
+		sum = sum + std::stod(art.ATotal);
 		if (kol*cjn != ukp) //provjera prve greske
 		{
 			std::ofstream errortxt(errorfilestring, std::ios::app); // otvaranje fajla u koji se smjestaju informacije o greskama
-			errortxt << "Racun sadrzi gresku-kolicina*cijena artikla nije dobro izracunata." << std::endl;
+			errortxt << "Racun sadrzi gresku-Kolicina*Cijena artikla nije dobro izracunata." << std::endl;
 			errflag = 1;
 		}
 	}
-	std::getline(fp, pom);
-	fp >> pom;
-	fp >> racUkupno;
-	sumR = std::stod(racUkupno);
+	std::getline(fp, tmp);
+	fp >> tmp;
+	fp >> billTotal;
+	sumR = std::stod(billTotal);
 	if (sum != sumR) // provjera druge greske
 	{
 		std::ofstream errortxt(errorfilestring, std::ios::app); // otvaranje fajla u koji se smjestaju informacije o greskama
 		errortxt << "Racun sadrzi gresku-ukupna vrijednost(bez PDV-a) nije dobro izracunata." << std::endl;
 		errflag=1;
 	}
-	std::getline(fp, pom);
-	fp >> pom;
+	std::getline(fp, tmp);
+	fp >> tmp;
 	fp >> PDV;
 	sumPDV = std::stod(PDV);
 	if (sumPDV != sumR*0.17) // provjera trece greske
@@ -65,12 +65,12 @@ bool checkError(string& file) // funkcija za provjeru greski
 		errortxt << "Racun sadrzi gresku-PDV nije dobro izracunat." << std::endl;
 		errflag=1;
 	}
-	std::getline(fp, pom);
-	fp >> pom;
-	fp >> pom;
-	fp >> pom;
-	fp >> ukupPDV;
-	sumUkup = std::stod(ukupPDV);
+	std::getline(fp, tmp);
+	fp >> tmp;
+	fp >> tmp;
+	fp >> tmp;
+	fp >> billTotalPDV;
+	sumUkup = std::stod(billTotalPDV);
 	if (sumUkup != sumR + sumPDV) // provjera cetvrte greske
 	{
 		std::ofstream errortxt(errorfilestring, std::ios::app); // otvaranje fajla u koji se smjestaju informacije o greskama
@@ -94,58 +94,58 @@ bool checkError(string& file) // funkcija za provjeru greski
 void format1(std::string& file)
 {
 	std::ifstream fp("Racuni/" + file); // otvaranje fajla koji sadrzi informacije
-	string pom,Kupac,racUkupno,PDV,ukupPDV,datum,pom2,pom3,stringart,stringkup;
+	string tmp,BBuyer,billTotal,PDV,billTotalPDV,BDate,tmp2,tmp3,stringart,stringkup;
 	Artikal art;
 	int flag = 1,datec=0;
 	if (checkError(file) == true) return; // provjera postojanja greske u racunu
-	fp >> pom; 
-	std::getline(fp, Kupac); // uzimanje imena kupca
-	trim(Kupac); //  brisanje viska prostora oko imena i prezimena
-	fp >> pom;
-	fp >> datum;
-	std::replace(datum.begin(), datum.end(), '/', '.'); //promjena svakog pojavljivanja / u . unutar datuma
-	std::getline(fp, pom);
-	std::getline(fp, pom);
-	std::getline(fp, pom);
-	std::getline(fp, pom); //preskakanje nebitnih linija
+	fp >> tmp; 
+	std::getline(fp, BBuyer); // uzimanje imena kupca
+	trim(BBuyer); //  brisanje viska prostora oko imena i prezimena
+	fp >> tmp;
+	fp >> BDate;
+	std::replace(BDate.begin(), BDate.end(), '/', '.'); //promjena svakog pojavljivanja / u . unutar datuma
+	std::getline(fp, tmp);
+	std::getline(fp, tmp);
+	std::getline(fp, tmp);
+	std::getline(fp, tmp); //preskakanje nebitnih linija
 	
 	while(flag==1) // petlja unutar koje se ucitavaju podaci
 	{
-		std::getline(fp, pom);
-		fp >> art.proizvod;
-		if (art.proizvod == "---------------------------------------")break; // uslov koji odredjuje da li postoji jos artikala u fajlu
-		fp >> art.sifra;
-		fp >> pom2;
-		fp >> art.kolicina;
-		fp >> pom2;
-		fp >> art.cijena;
-		fp >> pom2;
-		fp >> art.ukupno;
-		stringart = "Artikli/" + art.proizvod + " " + art.sifra + ".txt"; //pronalazak imena fajla u kome se cuvaju podaci za dati artikal
-		stringkup = "Kupci/"+ Kupac +".txt"; //pronalazak imena fajla u kome se cuvaju podaci za datog kupca
+		std::getline(fp, tmp);
+		fp >> art.AArticle;
+		if (art.AArticle == "---------------------------------------")break; // uslov koji odredjuje da li postoji jos artikala u fajlu
+		fp >> art.ACode;
+		fp >> tmp2;
+		fp >> art.AQuant;
+		fp >> tmp2;
+		fp >> art.APrice;
+		fp >> tmp2;
+		fp >> art.ATotal;
+		stringart = "Artikli/" + art.AArticle + " " + art.ACode + ".txt"; //pronalazak imena fajla u kome se cuvaju podaci za dati artikal
+		stringkup = "Kupci/"+ BBuyer +".txt"; //pronalazak imena fajla u kome se cuvaju podaci za datog kupca
 	   std::ofstream fileart(stringart,std::ios::app), filekup(stringkup,std::ios::app); //otvaranje fajlova u append modu
-		fileart << Kupac << std::endl;
-		fileart << datum << std::endl;
-		fileart << art.kolicina << " " << art.cijena << " " << art.ukupno << std::endl; //upis podataka u file vezan za artikal
+		fileart << BBuyer << std::endl;
+		fileart << BDate << std::endl;
+		fileart << art.AQuant << " " << art.APrice << " " << art.ATotal << std::endl; //upis podataka u file vezan za artikal
 		fileart << "---------------------------------------" << std::endl;
-		if (datec == 0) { filekup << datum <<"."<< std::endl; datec++; } // provjera koja osigura da ce se datum samo jednom unijeti u fajl za sve artikle kupljenje u istom danu
-		filekup <<art.proizvod<< " " << art.sifra << " " << art.kolicina << " " << art.cijena << " " << art.ukupno << std::endl; //unos podataka u fajl kupca
+		if (datec == 0) { filekup << BDate <<"."<< std::endl; datec++; } // provjera koja osigura da ce se datum samo jednom unijeti u fajl za sve artikle kupljenje u istom danu
+		filekup <<art.AArticle<< " " << art.ACode << " " << art.AQuant << " " << art.APrice << " " << art.ATotal << std::endl; //unos podataka u fajl kupca
 	}
-	std::getline(fp, pom);
-	fp >> pom;
-	fp >> racUkupno;
-	std::getline(fp, pom);
-	fp >> pom;
+	std::getline(fp, tmp);
+	fp >> tmp;
+	fp >> billTotal;
+	std::getline(fp, tmp);
+	fp >> tmp;
 	fp >> PDV;
-	std::getline(fp, pom);
-	fp >> pom;
-	fp >> pom;
-	fp >> pom;
-	fp >> ukupPDV; //ucitavanje zadnjih podataka u fajlu,ukupne vrijednosti,PDVa i ukupne vrijednosti sa PDV-om
+	std::getline(fp, tmp);
+	fp >> tmp;
+	fp >> tmp;
+	fp >> tmp;
+	fp >> billTotalPDV; //ucitavanje zadnjih podataka u fajlu,ukupne vrijednosti,PDVa i ukupne vrijednosti sa PDV-om
 	std::ofstream filekup(stringkup, std::ios::app);
-	filekup << racUkupno << std::endl;
+	filekup << billTotal << std::endl;
 	filekup << PDV << std::endl;
-	filekup << ukupPDV << std::endl;
+	filekup << billTotalPDV << std::endl;
 	filekup << "---------------------------------------" << std::endl; //upis podataka u fajl za kupca
     fp.seekg(0, fp.beg); //vracanje na pocetak datoteke
 	std::ofstream done("Obradjeni_racuni/" + file); //pravljenje kopije racuna u folderu za obradjene racune
